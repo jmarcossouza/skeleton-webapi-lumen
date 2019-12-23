d0b4e44f0a2655239ea1067584c86cc4efdb52b9
João Marcos A. Souza <contato@jmarcossouza.com>
2019-12-23 11:15:36 -0300

    Tabelas de log

---
+++ database/migrations/2019_12_23_105245_create_acoes_log_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAcoesLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('acoes_log', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('acao', 100);
+        });
+
+        $this->insert();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('acoes_log');
+    }
+
+    /**
+     * Insere registros na tabela
+     *
+     * @return void
+     */
+    private function insert()
+    {
+        DB::table('acoes_log')->insertOrIgnore([
+            ['acao' => 'Novo usuário'],
+            ['acao' => 'Login'],
+            ['acao' => 'Tentativa falha de login'],
+            ['acao' => 'Pedido redefinição de senha'],
+            ['acao' => 'Redefinição de senha'],
+            //Você pode e deve adicionar outras ações aqui...
+        ]);
+    }
+}
---
+++ database/migrations/2019_12_23_110834_create_logs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->bigInteger('usuario_id')->comment('ID do usuário, referência à tabela de usuarios');
+            $table->dateTime('data', 3)->default(DB::raw('CURRENT_TIMESTAMP(3)')); //Se quiser mudar a precisão dos segundos, mudo o segundo parâmetro do dateTime() e também o raw('current_timestamp(..))
+            $table->string('ip', 45)->comment('IP do usuário, pode ser tanto IPV6 quanto IPV4');
+            $table->smallInteger('acao_id')->unsigned()->comment('ID da ação realizada, olhar na tabela acoes_log');
+
+            $table->primary(['usuario_id', 'data']);
+            // $table->foreign('usuario_id')->references('id')->on('usuarios');
+            $table->foreign('acao_id')->references('id')->on('acoes_log')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
