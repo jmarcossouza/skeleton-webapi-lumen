ba8c1abbada3dd73d7069ac775b74a7aef2731ac
João Marcos A. Souza <contato@jmarcossouza.com>
2019-12-24 08:38:55 -0300

    v_logs

---
+++ database/migrations/2019_12_24_091711_create_v_log_view.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVLogView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("CREATE VIEW v_logs AS
+            select logs.usuario_id, logs.data, logs.ip, acoes_log.acao 
+            from logs 
+            inner join acoes_log on acoes_log.id = logs.acao_id
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement("drop view v_logs");
+    }
+}
